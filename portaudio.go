f6323ebb61109986f87b3a1a82b9dc7c4974ef25
AnyCPU <AnyCPU@users.noreply.github.com>
2025-09-19 23:17:10 +0200

    portaudio: do not use pkg-config

--- portaudio.go
+++ portaudio.go
@@ -12,7 +12,6 @@ The StreamParameters struct combines parameters for both the input and the outpu
 package portaudio
 
 /*
-#cgo pkg-config: portaudio-2.0
 #include <portaudio.h>
 extern PaStreamCallback* paStreamCallback;
 */
